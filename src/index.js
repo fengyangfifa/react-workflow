8fecf373805217c1452180d7f55dd09b41af2444
fengyangfifa <2314034614@qq.com>
2022-07-31 23:16:30 +0800

    feat: 添加 babel 处理 es6、7 等高级语法

---
+++ src/index.js
@@ -0,0 +1,15 @@
+class Example {
+  constructor() {
+    console.log('sss')
+  }
+}
+
+const a = new Example()
+console.log(a);
+
+const bbb = '123'
+console.log(bbb)
+
+const funa = () => {
+  console.log('funca')
+}
